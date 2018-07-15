cf26b3d0669e219697f63613e919ca5dedee5614
giantthinker <ramadanabdel24@gmail.com>
2018-07-15 13:52:15 +0100

    My blog skeleton

---
+++ footer.php
@@ -0,0 +1,4 @@
+<script type="text/javascript" src="assets/js/script.js" ></script>
+<footer class="container">
+      <p>&copy; Abdellah 2018</p>
+</footer>
---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+$title = 'Home';
+
+include 'head/header.php';
+?>
+
+</div>
+</div>
+
+<div class="container">
+
+    <div class="row" align="center">
+        <div class="col">
+          <h1 class="ui header heading">Welcome to my Blog</h1>
+            <p>You can find everything about me in this blog and also things related to technology.
+                Check out regularly for daily updates and more.
+            </p>
+        </div>
+      
+    </div>
+   
+     
+
+        <!-- Example row of columns -->
+        <div class="row">
+          <div class="col-md-4">
+            <h3>Projects</h3>
+            <p>GitHub the medium of software collaboration is one place I really enjoy when I pull and push codes
+                . Contributing in software is really a great thing to do as it enables me enjoy my passion.  
+            </p>
+            
+            <p><a class="ui primary basic button" href="#" role="button">Read More &raquo;</a></p>
+          </div>
+          <div class="col-md-4">
+            <h3>Coding in JS</h3>
+            <p>I've never been active when it came to JavaScript one of the most popular language on the web today.
+              This was because, I had no concentration learning a particular language and was learning at in one.
+            </p>
+            <p><a class="ui primary basic button" href="#" role="button">Read More &raquo;</a></p>
+          </div>
+          <div class="col-md-4">
+            <h3>PHP Projects</h3>
+            <p>Sample posts contents </p>
+            <p><a class="ui primary basic button" href="#" role="button">Read More &raquo;</a></p>
+          </div>
+        </div>
+
+        <hr>
+
+      </div> <!-- /container -->
+
+<?php
+
+
+include 'footer.php';
+
+?>
