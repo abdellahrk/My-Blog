6c3b85b7c13cdfd8df4bbe5e845946c2ae89c8d2
giantthinker <ramadanabdel24@gmail.com>
2018-07-16 20:17:19 +0100

    Add the post page and post to Database

--- querydb.php
+++ querydb.php
@@ -1,12 +1,13 @@
 <?php
 
 /* database connection */
-
+require 'credentials.php';
 require 'Posts.php';
 
 
+
 try{
-    $pdo = new PDO("mysql:host=localhost;dbname=blog", 'dolphin', 'iamdolphin');
+    $pdo = new PDO("mysql:host=localhost;dbname=blog", $username, $password);
     //echo "connected successfully";
 
 } catch(PDOException $e){
